794c073fd9c4673d07212e248b5e42e59f0a76a4
burokoron <sapatapata831@gmail.com>
2021-09-28 00:02:54 +0900

    fix：フォーマット修正

--- utils/rating.py
+++ utils/rating.py
@@ -3,22 +3,28 @@
 """
 
 import random
+import typing
 
-fixed_rating_players = {  # 基準エンジンのレーティングとそれに対する勝率
+# 基準エンジンのレーティングとそれに対する勝率
+fixed_rating_players: typing.Dict[str, typing.List[float]] = {
     "elo_rating": [166, 400, 750],
-    "win_rate": [0.528, 0.014, 0.0]
+    "win_rate": [0.528, 0.014, 0.0],
 }
-
-my_elo_rating = min(fixed_rating_players["elo_rating"])
+# イテレーション
 iteration = 1000000
 
+my_elo_rating = min(fixed_rating_players["elo_rating"])
 
-def update(my_elo_rating, iteration):
+
+def update(my_elo_rating: float, iteration: int) -> typing.Tuple[float, float]:
     min_rating = my_elo_rating
     max_reting = my_elo_rating
     for _ in range(iteration):
         for i in range(len(fixed_rating_players["elo_rating"])):
-            w = 1 / (10 ** (abs(fixed_rating_players["elo_rating"][i] - my_elo_rating) / 400) + 1)
+            w = 1 / (
+                10 ** (abs(fixed_rating_players["elo_rating"][i] - my_elo_rating) / 400)
+                + 1
+            )
             rand = random.random()
             if rand < fixed_rating_players["win_rate"][i]:
                 if my_elo_rating < fixed_rating_players["elo_rating"][i]:
@@ -37,7 +43,7 @@ def update(my_elo_rating, iteration):
 
 
 c = my_elo_rating
-while(c > 1):
+while c > 1:
     a, b = update(my_elo_rating=my_elo_rating, iteration=iteration)
     c = abs(my_elo_rating - (a + b) / 2)
     my_elo_rating = (a + b) / 2
